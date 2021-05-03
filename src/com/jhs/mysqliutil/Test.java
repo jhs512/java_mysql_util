d030e356be5ceba5a75abc7755428824ed3f0d63
jhs512 <jangka512@gmail.com>
2021-05-03 21:17:01 +0900

    잭슨 기본예제

--- src/com/jhs/mysqliutil/Test.java
+++ src/com/jhs/mysqliutil/Test.java
@@ -3,8 +3,40 @@ package com.jhs.mysqliutil;
 import java.util.List;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Test {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws JsonMappingException, JsonProcessingException {
+		// mysqlUtilTest();
+
+		jacksonTest();
+	}
+
+	private static void jacksonTest() throws JsonMappingException, JsonProcessingException {
+		ObjectMapper om = new ObjectMapper();
+
+		// jsonString to Map
+		// {"id":1, "title":"제목"}
+		String jsonStr1 = "{\"id\":1,\"title\":\"제목\",\"body\":\"내용\",\"memberId\":1}";
+		Map map1 = om.readValue(jsonStr1, Map.class);
+		System.out.println("map1.get(\"id\") : " + map1.get("id"));
+		System.out.println("(int)map1.get(\"id\") + 1 : " + ((int) map1.get("id") + 1));
+		System.out.println("map1.get(\"title\") : " + map1.get("title"));
+
+		// jsonString to Article
+		// {"id":1, "title":"제목"}
+		Article article1 = om.readValue(jsonStr1, Article.class);
+		System.out.println(article1);
+
+		// map to Article
+		Article articl2 = om.convertValue(map1, Article.class);
+		System.out.println(articl2);
+	}
+
+	private static void mysqlUtilTest() {
 		MysqlUtil.setDBInfo("localhost", "sbsst", "sbs123414", "mysqlutil");
 
 		MysqlUtil.setDevMode(true);
@@ -76,5 +108,17 @@ public class Test {
 		System.out.println("count : " + count);
 
 		MysqlUtil.closeConnection();
+	}
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class Article {
+	public int id;
+	public String title;
+	public String body;
+
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", title=" + title + ", body=" + body + "]";
 	}
 }
